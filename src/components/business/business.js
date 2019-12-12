095f07bbf55cd0050d8169b3470c001df7e9790c
Tomasz Flakus <tomasz.flakus@gmail.com>
2019-12-12 20:30:03 +0100

    aaa

--- src/components/business/business.js
+++ src/components/business/business.js
@@ -18,10 +18,7 @@ class Business extends React.Component {
     return (
       <div className="Business">
         <div className="image-container">
-          <img
-            src="https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg"
-            alt=""
-          />
+          <img src={"business.imageSrc"} alt="" />
         </div>
         <h2>MarginOtto Pizzeria</h2>
         <div className="Business-information">
@@ -40,3 +37,4 @@ class Business extends React.Component {
     );
   }
 }
+export default Business;
